ee97365a336694dd5b55d313141ee65148c36d99
Donghyeon Shin <shindong0321@gmail.com>
2025-12-24 20:56:14 +0900

    build : shadnc 라이브러리 적용 및 Theme 설정

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,8 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Button } from "~/components/ui/button";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +10,8 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <div>
+    <h1>Home</h1>
+    <Button>Click me</Button>
+  </div>;
 }
