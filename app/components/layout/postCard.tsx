c1d56c4f328fcd5ce3d84159983d1651476f1d35
Donghyeon Shin <shindong0321@gmail.com>
2025-12-26 01:53:05 +0900

    feat : postCard 컴포넌트 구현

---
+++ app/components/layout/postCard.tsx
@@ -0,0 +1,54 @@
+import { cva } from 'class-variance-authority';
+import { format } from 'date-fns';
+import { ClockIcon, DotIcon } from 'lucide-react';
+import { Link } from 'react-router';
+
+const categoryVariants = cva('rounded-md border px-4 text-sm font-medium', {
+  variants: {
+    category: {
+      Algorithm: 'bg-[#1E293B] text-[#60A5FA]',
+      Book: 'bg-[#2A1F1D] text-[#F59E0B]',
+      LangChain: 'bg-[#052E2B] text-[#2DD4BF]',
+      React: 'bg-[#0F172A] text-[#38BDF8]',
+      Research: 'bg-[#1C1917] text-[#A78BFA]',
+      SQL: 'bg-[#1F2937] text-[#34D399]',
+      Project: 'bg-[#18181B] text-[#F472B6]',
+    },
+  },
+});
+
+export default function PostCard({
+  title,
+  description,
+  category,
+  date,
+  link,
+  readTime,
+}: {
+  title: string;
+  description: string;
+  category: 'Algorithm' | 'Book' | 'LangChain' | 'React' | 'Research' | 'SQL' | 'Project';
+  date: Date;
+  link: string;
+  readTime: number;
+}) {
+  const formattedDate = format(date, 'MMM dd, yyyy');
+
+  return (
+    <Link to={link} className='border rounded-lg p-8 flex flex-col gap-4 hover:bg-primary/10'>
+      <div className='flex flex-row gap-2 items-center text-muted-foreground'>
+        <div className={categoryVariants({ category })}>{category}</div>
+        <DotIcon className='size-5' />
+        <div className='text-muted-foreground text-sm'>{formattedDate}</div>
+      </div>
+      <div className='flex flex-col gap-2'>
+        <h2 className='text-2xl font-bold'>{title}</h2>
+        <p className='text-muted-foreground text-sm line-clamp-2'>{description}</p>
+      </div>
+      <div className='flex flex-row gap-2 items-center'>
+        <ClockIcon className='size-4 text-muted-foreground ' />
+        <div className='text-muted-foreground text-xs'>{readTime} min read</div>
+      </div>
+    </Link>
+  );
+}
--- app/pages/posts.tsx
+++ app/pages/posts.tsx
@@ -1,41 +1,106 @@
 import { NavLink } from 'react-router';
 import type { Route } from './+types/posts';
 import { cva } from 'class-variance-authority';
+import PostCard from '~/components/layout/postCard';
 
 export const loader = async ({ request }: Route.LoaderArgs) => {
   const url = new URL(request.url);
   const category = url.pathname.split('/')[2];
-  console.log(category);
   return { category };
 };
 
 const navLinkVariants = cva('rounded-full border px-4 py-1', {
   variants: {
     isActive: {
-      true: 'bg-primary font-medium text-primary-foreground',
+      true: 'bg-foreground font-medium text-background',
       false: 'hover:bg-primary/10',
     },
   },
 });
+
+type PostCategory = 'Algorithm' | 'Book' | 'LangChain' | 'React' | 'Research' | 'SQL' | 'Project';
+
+const categoryList: PostCategory[] = [
+  'Algorithm',
+  'Book',
+  'LangChain',
+  'React',
+  'Research',
+  'SQL',
+  'Project',
+];
+
+interface Post {
+  title: string;
+  description: string;
+  category: PostCategory;
+  date: Date;
+  link: string;
+  readTime: number;
+}
+
+const postList: Post[] = [
+  {
+    title: 'Post 1',
+    description: 'Post 1 description',
+    category: 'Algorithm',
+    date: new Date('2025-01-01'),
+    link: '/post/post-1',
+    readTime: 10,
+  },
+  {
+    title: 'Post 2',
+    description: 'Post 2 description',
+    category: 'Book',
+    date: new Date('2025-01-01'),
+    link: '/post/post-2',
+    readTime: 10,
+  },
+  {
+    title: 'Post 3',
+    description: 'Post 3 description',
+    category: 'LangChain',
+    date: new Date('2025-01-01'),
+    link: '/post/post-3',
+    readTime: 10,
+  },
+];
+
 export default function Posts() {
   return (
-    <div className='flex flex-col gap-8'>
+    <div className='flex flex-col gap-8 max-w-[1400px] md:ml-20'>
       <div className='flex flex-col gap-4'>
         <h1 className='text-4xl font-bold'>All Posts</h1>
         <p className='text-muted-foreground'>
           A collection of 42 articles on programming, technology and life.
         </p>
       </div>
-      <div className='flex flex-row gap-2'>
+      <div className='flex flex-row flex-wrap gap-2'>
         <NavLink to='/posts/all' className={({ isActive }) => navLinkVariants({ isActive })}>
           View All
         </NavLink>
-        <NavLink
-          to='/posts/development'
-          className={({ isActive }) => navLinkVariants({ isActive })}
-        >
-          Development
-        </NavLink>
+        {categoryList.map((category) => (
+          <NavLink
+            key={category}
+            to={`/posts/${category}`}
+            className={({ isActive }) => navLinkVariants({ isActive })}
+          >
+            {category}
+          </NavLink>
+        ))}
+      </div>
+      <div className='flex flex-col gap-4'>
+        {postList.map((post) => (
+          <PostCard
+            key={post.title}
+            title={post.title}
+            description={post.description}
+            category={post.category}
+            date={post.date}
+            link={post.link}
+            readTime={post.readTime}
+          />
+        ))}
       </div>
     </div>
   );
