0add1b3c802a8ef1166ab06334b24c00270c57a4
Donghyeon Shin <shindong0321@gmail.com>
2025-12-25 18:18:10 +0900

    feat : Sidebar Now(Create, Update) 목록 기능 추가

--- app/components/layout/leftSideBar.tsx
+++ app/components/layout/leftSideBar.tsx
@@ -1,6 +1,6 @@
 import { cva } from 'class-variance-authority';
 import { BookIcon, LayoutGrid, StarIcon } from 'lucide-react';
-import { NavLink } from 'react-router';
+import { Link, NavLink } from 'react-router';
 import FolderItems from './folderItems';
 import type { FolderItemProps } from '~/types/folderItemProps';
 
@@ -104,11 +104,48 @@ const foloderList: FolderItemProps[] = [
   },
 ];
 
+interface NowList {
+  type: 'update' | 'create';
+  posts: {
+    title: string;
+    link: string;
+    description: string;
+  }[];
+}
+
+const nowList: NowList[] = [
+  {
+    type: 'update',
+    posts: [
+      {
+        title: 'Update 1',
+        link: '/posts/update-1',
+        description: 'Update 1 description',
+      },
+      {
+        title: 'Update 2',
+        link: '/posts/update-2',
+        description: 'Update 2 description',
+      },
+    ],
+  },
+  {
+    type: 'create',
+    posts: [
+      {
+        title: 'Create 1',
+        link: '/posts/create-1',
+        description: 'Create 1 description',
+      },
+    ],
+  },
+];
+
 export default function LeftSidebar() {
   return (
     <div className='hidden md:block'>
       <div className='flex flex-col gap-10 justify-center'>
-        <div className='flex flex-col gap-2'>
+        <div className='flex flex-col gap-4'>
           <h1 className='text-sm font-medium text-muted-foreground/50'>DISCOVER</h1>
           <div className='flex flex-col gap-2'>
             <NavLink
@@ -129,7 +166,46 @@ export default function LeftSidebar() {
             </NavLink>
           </div>
         </div>
-        <div className='flex flex-col gap-2'>
+        {nowList && nowList.length > 0 && (
+          <div className='flex flex-col gap-4'>
+            <h1 className='text-sm font-medium text-muted-foreground/50'>NOW</h1>
+            <div className='border rounded-md p-2 bg-muted-foreground/5 flex flex-col gap-5'>
+              {nowList.map((item) => (
+                <div key={item.type} className='flex flex-col gap-2 ml-2'>
+                  <div className='flex items-center gap-2'>
+                    <span className='relative flex h-3 w-3'>
+                      {item.type === 'update' ? (
+                        <>
+                          <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75'></span>
+                          <span className='relative inline-flex rounded-full h-3 w-3 bg-green-500'></span>
+                        </>
+                      ) : (
+                        <>
+                          <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-400 opacity-75'></span>
+                          <span className='relative inline-flex rounded-full h-3 w-3 bg-purple-500'></span>
+                        </>
+                      )}
+                    </span>
+                    <span className='text-muted-foreground font-medium text-sm capitalize'>
+                      {item.type}
+                    </span>
+                  </div>
+                  <div className='flex flex-col gap-2'>
+                    {item.posts.map((post) => (
+                      <Link to={post.link} key={post.title} className='flex flex-col gap-1'>
+                        <span className='font-medium text-xs'>{post.title}</span>
+                        <span className='text-muted-foreground font-medium text-xs line-clamp-2'>
+                          {post.description}
+                        </span>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+        <div className='flex flex-col gap-4'>
           <h1 className='text-sm font-medium text-muted-foreground/50'>CATEGORIES</h1>
           <div className='flex flex-col gap-1'>
             {foloderList.map((item) => (
