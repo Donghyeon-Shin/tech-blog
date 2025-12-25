a3a3de028015776c01599858327857b9a78a81e3
Donghyeon Shin <shindong0321@gmail.com>
2025-12-26 05:50:54 +0900

    feat : Popular Posts Page 반응형 구현

--- app/components/ui/popularPostCard.tsx
+++ app/components/ui/popularPostCard.tsx
@@ -4,19 +4,22 @@ import { DotIcon, EyeIcon } from 'lucide-react';
 import { Link } from 'react-router';
 import { Separator } from '~/components/ui/separator';
 
-const categoryVariants = cva('rounded-md border px-4 text-sm font-medium', {
-  variants: {
-    category: {
-      Algorithm: 'bg-[#1E293B] text-[#60A5FA]',
-      Book: 'bg-[#2A1F1D] text-[#F59E0B]',
-      LangChain: 'bg-[#052E2B] text-[#2DD4BF]',
-      React: 'bg-[#0F172A] text-[#38BDF8]',
-      Research: 'bg-[#1C1917] text-[#A78BFA]',
-      SQL: 'bg-[#1F2937] text-[#34D399]',
-      Project: 'bg-[#18181B] text-[#F472B6]',
+const categoryVariants = cva(
+  'rounded-md border px-2 md:px-4 text-xs md:text-sm font-medium shrink-0',
+  {
+    variants: {
+      category: {
+        Algorithm: 'bg-[#1E293B] text-[#60A5FA]',
+        Book: 'bg-[#2A1F1D] text-[#F59E0B]',
+        LangChain: 'bg-[#052E2B] text-[#2DD4BF]',
+        React: 'bg-[#0F172A] text-[#38BDF8]',
+        Research: 'bg-[#1C1917] text-[#A78BFA]',
+        SQL: 'bg-[#1F2937] text-[#34D399]',
+        Project: 'bg-[#18181B] text-[#F472B6]',
+      },
     },
   },
-});
+);
 
 export default function PopularPostCard({
   title,
@@ -44,7 +47,7 @@ export default function PopularPostCard({
       to={link}
       className='border rounded-lg p-8 flex flex-col gap-4 hover:bg-primary/10 relative'
     >
-      <div className='absolute top-5 right-5'>
+      <div className='absolute bottom-5 right-5 lg:top-5'>
         <div className='flex flex-row gap-5 items-center h-16'>
           <Separator orientation='vertical' className='h-16' />
           <div className='text-muted text-4xl font-extrabold tracking-wider'>
@@ -52,12 +55,16 @@ export default function PopularPostCard({
           </div>
         </div>
       </div>
-      <div className='flex flex-row gap-1 items-center text-muted-foreground'>
+      <div className='flex flex-row gap-1 items-center text-muted-foreground w-full flex-nowrap'>
         <div className={categoryVariants({ category })}>{category}</div>
-        <DotIcon className='size-5' />
-        <div className='text-muted-foreground text-sm'>{readTime} min read</div>
-        <DotIcon className='size-5' />
-        <div className='text-muted-foreground text-sm'>{formattedDate}</div>
+        <DotIcon className='size-4 shrink-0' />
+        <div className='text-muted-foreground text-xs whitespace-nowrap shrink-0'>
+          {readTime} min read
+        </div>
+        <DotIcon className='size-4 shrink-0' />
+        <div className='text-muted-foreground text-xs whitespace-nowrap shrink-0'>
+          {formattedDate}
+        </div>
       </div>
       <div className='flex flex-col gap-2'>
         <h2 className='text-2xl font-bold'>{title}</h2>
