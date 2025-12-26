02873f11788d85884258c8b1c74a92cfdedf572d
Donghyeon Shin <shindong0321@gmail.com>
2025-12-26 22:50:35 +0900

    feat : postStats trigger 구현

---
+++ app/sql/triggers/post_stats_trigger.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION public.update_post_daily_stats()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+SECURITY definer
+SET search_path = ''
+AS $$
+BEGIN
+    IF ( OLD.view_count IS DISTINCT FROM NEW.view_count ) THEN
+        INSERT INTO public.post_stats (post_id, category_id, daily_view_count, record_date) 
+        VALUES (
+            NEW.post_id, 
+            NEW.category_id, 
+            1, 
+            NOW()
+        )
+        ON CONFLICT (post_id, record_date) 
+        DO UPDATE SET daily_view_count = daily_view_count + 1;
+    END IF;
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER post_stats_trigger
+AFTER UPDATE ON public.posts
+FOR EACH ROW
+EXECUTE FUNCTION public.update_post_daily_stats();
