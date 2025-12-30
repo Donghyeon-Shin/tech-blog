8569c83b466caabe239db7cc08f3fa6d0b3fa174
Donghyeon Shin <shindong0321@gmail.com>
2025-12-30 23:49:23 +0900

    feat : Left SideBar Layout 분리

---
+++ app/components/layout/leftSidebarLayout.tsx
@@ -0,0 +1,24 @@
+import { Outlet, useOutletContext } from 'react-router';
+import LeftSidebar from './leftSideBar';
+import type { FolderItemProps } from '~/types/folderItemProps';
+import type { Route } from '../../+types/root';
+
+export default function LeftSidebarLayout() {
+  const { loaderData } = useOutletContext<{
+    loaderData: Route.ComponentProps['loaderData'];
+  }>();
+  return (
+    <div className='mx-auto xl:mx-20 grid grid-cols-1 md:grid-cols-[280px_1fr] xl:grid-cols-[280px_1fr] gap-8 px-6 py-8'>
+      <LeftSidebar
+        categoriesTree={loaderData.categoriesTree as FolderItemProps[]}
+        events={loaderData.events}
+      />
+      <Outlet
+        context={{
+          topLevelCategories: loaderData?.topLevelCategories,
+          allCategories: loaderData?.categories,
+        }}
+      />
+    </div>
+  );
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,12 +1,14 @@
-import { type RouteConfig, index, prefix, route } from '@react-router/dev/routes';
+import { type RouteConfig, index, layout, prefix, route } from '@react-router/dev/routes';
 
 export default [
-  index('pages/index.tsx'),
-  ...prefix('posts', [route('/:category', 'pages/posts.tsx')]),
-  route('/popular', 'pages/popularPosts.tsx'),
-  route('/post/:id', 'pages/post.tsx'),
-  route('/about', 'pages/about.tsx'),
-  route('/dashboard', 'pages/dashboard.tsx'),
+  layout('components/layout/leftSidebarLayout.tsx', [
+    index('pages/index.tsx'),
+    ...prefix('posts', [route('/:category', 'pages/posts.tsx')]),
+    route('/popular', 'pages/popularPosts.tsx'),
+    route('/post/:id', 'pages/post.tsx'),
+    route('/about', 'pages/about.tsx'),
+    route('/dashboard', 'pages/dashboard.tsx'),
+  ]),
   // prettier-ignore
   ...prefix('api', [
     ...prefix('settings', [
