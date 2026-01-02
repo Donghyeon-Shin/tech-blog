becafed64076ca8505824488ef8f3a464feb5aef
Donghyeon Shin <shindong0321@gmail.com>
2026-01-03 00:26:38 +0900

    feat : about Page의 Tech Stack 뱃지 색 변경

--- app/components/ui/techBadge.tsx
+++ app/components/ui/techBadge.tsx
@@ -1,6 +1,6 @@
 export default function TechBadge({ title }: { title: string }) {
   return (
-    <h4 className='text-xs bg-primary/50 text-primary-foreground border rounded-md px-4 py-2'>
+    <h4 className='text-xs bg-accent/50 text-accent-foreground border rounded-md px-4 py-2'>
       {title}
     </h4>
   );
