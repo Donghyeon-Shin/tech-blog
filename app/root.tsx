8092ffcae030e778ad129a3e4dcca9d9204d0460
Donghyeon Shin <shindong0321@gmail.com>
2026-01-04 00:00:22 +0900

    feat : nporgress type 정의 라이브러리 설치 및 적용

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,8 @@ import { getAllPostsForBuildingCategoriesTree } from './api/posts/posts-api';
 import { buildCategoriesTree } from './lib/buildCategoriesTree';
 import { getEvents } from './api/events/events-api';
 import { cn } from './lib/utils';
+import nProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 export const links: Route.LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -147,6 +149,14 @@ export function InnerLayout({ children }: { children: React.ReactNode }) {
 export default function App({ loaderData }: Route.ComponentProps) {
   const navigation = useNavigation();
   const isLoading = navigation.state === 'loading';
+
+  useEffect(() => {
+    if (isLoading) {
+      nProgress.start();
+    } else {
+      nProgress.done();
+    }
+  }, [isLoading]);
 
   return (
     <div className={cn(isLoading && 'opacity-70 pointer-events-none cursor-wait')}>
